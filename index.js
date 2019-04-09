58ce1adc8c8cd28afd73b61043c5f42eac0a68a5
Blake Rose <bdecktor@gmail.com>
2019-04-09 03:03:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function whisper(string) {return string.toLowerCase ()}
 function logShout(string) {return console.log(string.toUpperCase ())}
 function logWhisper(string) {return console.log(string.toLowerCase ())}
 function sayHiToGrandma(string) {
-  if (string.toLowerCase) {return "I can\'t hear you!"} 
+  if (string === string.toLowerCase) {return "I can\'t hear you!"} 
   if (string === string.toUpperCase) {return "YES INDEED!"}
   else if (string === "I love you, Grandma.") {return "I love you, too."}}
 

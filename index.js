d35d50ffed1b5991800a15372a28851b41e402f4
Blake Rose <bdecktor@gmail.com>
2019-04-09 02:34:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ lowercase.toLowerCase()===lowercase
 var misedCase = 'Hi there!'
 
 function shout(string) {return string.toUpperCase () }
-function logWhisper(string) {return string.toLowerCase ("HELLO")}
+function whisper(string) {return string.toLowerCase}
 
 function logShout (string) {return console.log(string.toUppercase)+logshout("Hello!")}
 function sayHiToGrandma(string) {

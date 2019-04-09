db78f0443e548ca86d9bcdc86f05c297cdfb0980
Blake Rose <bdecktor@gmail.com>
2019-04-09 02:48:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var logShout = 'hello!'
 var logWhisper = 'hello' 
+var uppercase = 'HELLO!'
+var lowercase = 'hello!'
 
 function shout(string) {return string.toUpperCase () }
 function whisper(string) {return string.toLowerCase ()}

26d176d46c331afa8f1afd7a555a3605fca841b1
Blake Rose <bdecktor@gmail.com>
2019-04-09 03:15:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var lowercase= 'hell!'
 function shout(string) {return string.toUpperCase () }
 function whisper(string) {return string.toLowerCase ()}
 function logShout(string) {return console.log(string.toUpperCase ())}

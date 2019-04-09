9f452d4e06c240801710550ebdabdf8f7d294aad
Blake Rose <bdecktor@gmail.com>
2019-04-09 03:21:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ function logShout(string) {return console.log(string.toUpperCase ())}
 function logWhisper(string) {return console.log(string.toLowerCase ())}
 
 function sayHiToGrandma(string) {
-  if (string === string.toUpperCase) {return "YES INDEED!"}}
-{if (string === string.toLowerCase) {return "I can\'t hear you!"}}
-function sayHiToGrandma(string) {
-  if (string === "I love you, Grandma.") {return "I love you, too."}}
+  if (string === string.toUpperCase) {return "YES INDEED!"}
+if (string === string.toLowerCase) {return "I can\'t hear you!"}
+if (string === "I love you, Grandma.") {return "I love you, too."}}

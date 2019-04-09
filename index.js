2f62c0780438bb329e87cd5ec99dfde63ce932cc
Blake Rose <bdecktor@gmail.com>
2019-04-09 03:16:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function shout(string) {return string.toUpperCase () }
 function whisper(string) {return string.toLowerCase ()}
 function logShout(string) {return console.log(string.toUpperCase ())}
 function logWhisper(string) {return console.log(string.toLowerCase ())}
-var lowercase= 'I can\'t hear you.'
+
 function sayHiToGrandma(string) {
   if (string === string.toUpperCase) {return "YES INDEED!"}}
 function sayHiToGrandma(string) { 
